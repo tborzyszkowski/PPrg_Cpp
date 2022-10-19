cdc4d470879877012670af557fab2a6271053601
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2022-10-19 23:17:47 +0200

    while i do-while

--- PPRG_02/Loops/Loops.cpp
+++ PPRG_02/Loops/Loops.cpp
@@ -5,15 +5,47 @@ using std::endl;
 
 int main()
 {
+	//// Suma 1 .. N
+	//// 
+	//const int N = 10;
+
+	//int result(0);
+
+	//for (int i = 0; i < N; i++) {
+	//	result += i;
+	//}
+
+	//cout << "Suma 1 .. " << N << " = " << result << endl;
+
+
+	//// Suma 1 .. N
+	//// wersja z pêtl¹ while
+	////
+	//const int N = 10;
+
+	//int result(0);
+	//int i(0);
+
+	//while (i < N) {
+	//	result += i;
+	//	i++;
+	//}
+
+	//cout << "Suma 1 .. " << N << " = " << result << endl;
+
+	// Suma 1 .. N
+	// wersja z pêtl¹ do while
+	//
 	const int N = 10;
 
 	int result(0);
+	int i(0);
 
-	for (int i = 0; i < N; i++) {
+	do {
 		result += i;
-	}
+		i++;
+	} while (i < N);
 
 	cout << "Suma 1 .. " << N << " = " << result << endl;
-
 }
 
