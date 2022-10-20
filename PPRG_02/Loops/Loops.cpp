6c9372ee313d8200bd43b8064cdc330c8cb003fd
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2022-10-20 22:31:27 +0200

    switch i pętle zagnieżdżone

--- PPRG_02/Loops/Loops.cpp
+++ PPRG_02/Loops/Loops.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using std::cout;
+using std::cin;
 using std::endl;
 
 
@@ -33,19 +34,112 @@ int main()
 
 	//cout << "Suma 1 .. " << N << " = " << result << endl;
 
-	// Suma 1 .. N
-	// wersja z pêtl¹ do while
+	//// Suma 1 .. N
+	//// wersja z pêtl¹ do while
+	////
+	//const int N = 10;
+
+	//int result(0);
+	//int i(0);
+
+	//do {
+	//	result += i;
+	//	i++;
+	//} while (i < N);
+
+	//cout << "Suma 1 .. " << N << " = " << result << endl;
+
+	////
+	//// przyk³ad_1: switch()
+	////
+	//char oper;
+	//float num1, num2;
+	//cout << "Enter an operator (+, -, *, /): ";
+	//cin >> oper;
+	//cout << "Enter two numbers: " << endl;
+	//cin >> num1 >> num2;
+
+	//switch (oper) {
+	//case '+':
+	//	cout << num1 << " + " << num2 << " = " << num1 + num2;
+	//	break;
+	//case '-':
+	//	cout << num1 << " - " << num2 << " = " << num1 - num2;
+	//	break;
+	//case '*':
+	//	cout << num1 << " * " << num2 << " = " << num1 * num2;
+	//	break;
+	//case '/':
+	//	cout << num1 << " / " << num2 << " = " << num1 / num2;
+	//	break;
+	//default:
+	//	cout << "Error! The operator is not correct";
+	//	break;
+	//}
+
+	////
+	//// przyk³ad_2: switch()
+	////
+	//short dzien;
+
+	//cout << "Podaj nr dnia tygodnia: ";
+	//cin >> dzien;
+
+	//switch (dzien)
+	//{
+	//case 1:
+	//case 2:
+	//case 3:
+	//case 4:
+	//case 5:
+	//	cout << "Podany dzieñ jest dniem roboczym";
+	//	break;
+	//case 6:
+	//case 7:
+	//	cout << "Mamy weekend";
+	//	break;
+	//default:
+	//	cout << "Nie ma takiego dnia";
+	//}
+
+	////
+	//// przyk³ad_1: zagnie¿d¿ona pêtla
+	////
+	//int ile = 0;
+
+	//for (int i = 1; i <= 9; i++)
+	//	for (int j = 0; j <= 9; j++)
+	//		for (int k = 0; k <= 9; k++)
+	//			if (i != j && i != k && j != k)
+	//			{
+	//				cout << i << j << k << " ";
+	//				ile++;
+	//			}
+
+	//cout << "Liczb trzycyfrowych o niepowtarzaj¹cych siê cyfrach jest: " << ile << endl;
+
 	//
-	const int N = 10;
+	// przyk³ad_2: zagnie¿d¿ona pêtla
+	//
+	int ile = 0, i = 1, j = 0, k = 0;
 
-	int result(0);
-	int i(0);
-
-	do {
-		result += i;
+	while (i <= 9) {
+		while (j <= 9) {
+			while (k <= 9) {
+				if (i != j && i != k && j != k) {
+					cout << i << j << k << " ";
+					ile++;
+				}
+				k++;
+			}
+			k = 0;
+			j++;
+		}
+		j = 0;
 		i++;
-	} while (i < N);
+	}
+	cout << "Liczb trzycyfrowych o niepowtarzaj¹cych siê cyfrach jest: " << ile << endl;
 
-	cout << "Suma 1 .. " << N << " = " << result << endl;
+	return 0;
 }
 
